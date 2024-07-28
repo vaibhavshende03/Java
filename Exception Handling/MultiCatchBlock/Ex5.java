b48ea9d012b2f42907022c0d2e797a7e8256983d
vaibhavshende03 <jitcsevaibhavshende@gmail.com>
2024-07-28 18:15:52 +0530

    MultiCatchBlock

--- Exception Handling/MultiCatchBlock/Ex5.java
+++ Exception Handling/MultiCatchBlock/Ex5.java
@@ -1,7 +1,7 @@
 package MultiCatchBlock;
 
 //Let's see an example, to handle the exception without maintaining the order of exceptions (i.e. from most specific to most general).
-
+//Gives error as we need to put the parent class (Exception class) after the child classes (Such as ArrayIndexOutOfBoundsException).
 public class Ex5 {
 
     public static void main(String[] args) {
